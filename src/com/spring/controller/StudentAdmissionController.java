1429f53883eec6602ad37145245c988a405a394e
Yin Kokpheng <yinkokpheng@yahoo.com>
2015-12-22 05:03:12 +0700

    new

--- src/com/spring/controller/StudentAdmissionController.java
+++ src/com/spring/controller/StudentAdmissionController.java
@@ -1,8 +1,15 @@
 package com.spring.controller;
 
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+import org.springframework.beans.propertyeditors.CustomDateEditor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.WebDataBinder;
+import org.springframework.web.bind.annotation.InitBinder;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -11,6 +18,16 @@ import org.springframework.web.servlet.ModelAndView;
 @Controller
 public class StudentAdmissionController {
 
+	@InitBinder
+	public void InitBinder(WebDataBinder binder){
+		//disable studentMobile field
+		binder.setDisallowedFields(new String[] {"studentMobile"});
+		
+		//register custom date format
+		SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy****mm****dd");
+		binder.registerCustomEditor(Date.class,"studentDOB", new CustomDateEditor(dateFormat, false));
+	}
+	
 	// This method will be call first before other method
 	@ModelAttribute
 	public void addCommonObjects(Model model) {
